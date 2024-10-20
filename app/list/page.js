36456bb4a177ab60c2497af01d6c64a80d33e426
공수한 <ksh92315@pionnet.co.kr>
2024-10-20 20:00:20 +0900

    상품리스트 좋아요 기능 개선

--- app/list/page.js
+++ app/list/page.js
@@ -4,7 +4,8 @@ import {useState} from "react";
 function List() {
 
     let 상품 = ['tomatoes', 'pasts', 'coconut'];
-    let [수량, 수량변경] = useState(1); //client component 에서만 사용가능
+    // let [수량, 수량변경] = useState(1); //client component 에서만 사용가능
+    let [상품_좋아요, 상품_좋아요_변경] = useState([0,0,0]);
     return (
         <>
             <div>
@@ -15,12 +16,16 @@ function List() {
                             <div className="food" key={i}>
                                 <img src={`/food${i}.png`} alt="과일" className="food-img"/>
                                 <h4>{g} $40</h4>
-                                <span> ❤️ :  {수량} </span>
+                                <span> ❤️ :  {상품_좋아요[i]} </span>
                                 <button onClick={() => { //client component 에서만 사용가능
-                                    수량변경(수량 + 1);
+                                    const NEW_상품_좋아요 = [...상품_좋아요];
+                                    NEW_상품_좋아요[i] += 1;
+                                    상품_좋아요_변경(NEW_상품_좋아요)
                                 }}>+</button>
                                 <button onClick={() => { //client component 에서만 사용가능
-                                    수량변경(수량 - 1);
+                                    const NEW_상품_좋아요 = [...상품_좋아요];
+                                    NEW_상품_좋아요[i] -= 1;
+                                    상품_좋아요_변경(NEW_상품_좋아요)
                                 }}>-</button>
                             </div>
                         )
