e7085d0682e4f08d65034fbcfdb96e2fa4d673c3
공수한 <ksh92315@pionnet.co.kr>
2024-10-20 19:55:04 +0900

    상품리스트 좋아요 기능 추가

--- app/list/page.js
+++ app/list/page.js
@@ -1,7 +1,10 @@
+'use client'
+import {useState} from "react";
+
 function List() {
 
-    let 상품 = ['tomatoes', 'pasts', 'coconut']
-
+    let 상품 = ['tomatoes', 'pasts', 'coconut'];
+    let [수량, 수량변경] = useState(1); //client component 에서만 사용가능
     return (
         <>
             <div>
@@ -12,6 +15,13 @@ function List() {
                             <div className="food" key={i}>
                                 <img src={`/food${i}.png`} alt="과일" className="food-img"/>
                                 <h4>{g} $40</h4>
+                                <span> ❤️ :  {수량} </span>
+                                <button onClick={() => { //client component 에서만 사용가능
+                                    수량변경(수량 + 1);
+                                }}>+</button>
+                                <button onClick={() => { //client component 에서만 사용가능
+                                    수량변경(수량 - 1);
+                                }}>-</button>
                             </div>
                         )
                     })
